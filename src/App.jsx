733c9e589f9ea06cc3f51a151ad7f7dd9730471d
Rethabileenno <rethabileenno@gmail.com>
2024-04-02 18:24:22 +0200

    Recipe details environment

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+// import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+   
+   <p>RECIPE DETAILS HERE </p>
+    </div>
+  );
+}
+
+export default App;
