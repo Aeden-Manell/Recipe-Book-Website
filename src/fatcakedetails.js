ba0e3420ed99c07b8eb1bf260eba7f6085ea4b3c
Rethabileenno <rethabileenno@gmail.com>
2024-04-04 01:03:41 +0200

    Added images, description and ingredients

--- src/fatcakedetails.js
+++ src/fatcakedetails.js
@@ -1,10 +1,20 @@
-import React from 'react';
+// import React from 'react';
 import StarRating from './StarRating';
-// import 'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css';
+import React, { useState } from 'react';
 import './App.css';
+import bigImage from './images/IMG_7183.jpg';
+
+
 
 
 const FatCakeDetails = () => {
+
+    const [isHeartRed, setHeartRed] = useState(false);
+
+    const toggleHeartColor = () => {
+      setHeartRed(!isHeartRed);
+    };
+
   return (
     <div>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css" />
@@ -23,8 +33,37 @@ const FatCakeDetails = () => {
       </div>
 
             <div className="product-title">Recipe Title</div>   
-            <div className='stars'> <StarRating rating={4} /> </div>
-          
+            
+            <div className='stars'>
+                <StarRating rating={4} />
+                <p>(99 reviews)</p>
+                </div>   
+                       
+    <div className='icons'>
+        <i className="fas fa-share-alt"> Share</i>
+        <i className={`fas fa-heart ${isHeartRed ? 'red' : ''}`} 
+          onClick={toggleHeartColor}
+        > Save</i>
+      </div>
+
+      <div className='image-grid'>
+                <img src={bigImage} alt='big' className='big-image' />
+            <img src={bigImage} alt='small-1' className='small-image' />
+            <img src={bigImage} alt='small-2' className='small-image' />
+            <img src={bigImage} alt='small-3' className='small-image' />
+            <img src={bigImage} alt='small-4' className='small-image' />
+            </div>
+
+            <div className='descriptionstitle'>
+            Recipe Description
+            <p>Bite through the crispy exterior of a freshly baked vetkoek into the soft goodness inside. <br></br>This is something every South African can identify with. This easy vetkoek recipe is simple to prepare and delicious.</p>
+            </div>
+
+            <ul class="ingredients-list">
+            <li><i class="fas fa-check-double"></i> Ingredient 1</li>
+            <li><i class="fas fa-check-double"></i> Ingredient 2</li>
+            <li><i class="fas fa-check-double"></i> Ingredient 3</li>
+            </ul>
             
 
  </div>
