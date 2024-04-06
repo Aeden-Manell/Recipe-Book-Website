2792f45aa0b1bf0dd3a7495172e6ac98c47f060e
Aeden Manell <nthabelengaedenmanell@gmail.com>
2024-04-06 19:52:51 +0200

    About Us Page

--- src/pages/OurStory.js
+++ src/pages/OurStory.js
@@ -1,20 +1,22 @@
 import React from "react";
-import Navbar from "../components/Navbar";
-import Footer from "../components/Footer";
-import About from "../components/About";
-import Newsletter from "../components/Newsletter";
-import HeroAbout from "../components/HeroAbout";
+import Navbar from "../components/Navbar"; // Import the Navbar component
+import Footer from "../components/Footer"; // Import the Footer component
+import About from "../components/About"; // Import the About component
+import Newsletter from "../components/Newsletter"; // Import the Newsletter component
+import HeroAbout from "../components/HeroAbout"; // Import the HeroAbout component
 
 const OurStory = () => {
-  return ( 
+  // This component represents the Our Story page of the website
+
+  return (
     <>
-      <Navbar/>
-      <HeroAbout/>
-      <About/>
-      <Newsletter/>
-      <Footer/>
+      <Navbar /> {/* Render the Navbar component */}
+      <HeroAbout /> {/* Render the HeroAbout component */}
+      <About /> {/* Render the About component */}
+      <Newsletter /> {/* Render the Newsletter component */}
+      <Footer /> {/* Render the Footer component */}
     </>
-   );
-}
- 
+  );
+};
+
 export default OurStory;
