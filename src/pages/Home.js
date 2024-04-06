7cd5cb51792b12d9ec3e663cb0b2ad778b272373
Aeden Manell <nthabelengaedenmanell@gmail.com>
2024-04-06 19:44:13 +0200

    home page

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,25 +1,26 @@
 import React from "react";
-import "./styles/home.css";
+import "./styles/home.css"; // Import the CSS file for the Home page
 
-import Navbar from "../components/Navbar";
-import Footer from "../components/Footer";
-import Hero from "../components/Hero";
-import Slider from "../components/Slider";
-import RecipeList from "../components/RecipeList";
-import HeadingText from "../components/HeadingText";
-import Newsletter from "../components/Newsletter";
+import Navbar from "../components/Navbar"; // Import the Navbar component
+import Footer from "../components/Footer"; // Import the Footer component
+import Hero from "../components/Hero"; // Import the Hero component
+import Slider from "../components/Slider"; // Import the Slider component
+import RecipeList from "../components/RecipeList"; // Import the RecipeList component
+import HeadingText from "../components/HeadingText"; // Import the HeadingText component
+import Newsletter from "../components/Newsletter"; // Import the Newsletter component
 
-export default function Home(){
+export default function Home() {
+  // This component represents the Home page of the website
+
   return (
-   <> 
-    <Navbar/>
-    <Hero/>
-    <Slider/>
-    <HeadingText/>
-    <RecipeList/>
-    <Newsletter/>
-    <Footer/>
-   </>
+    <>
+      <Navbar /> {/* Render the Navbar component */}
+      <Hero /> {/* Render the Hero component */}
+      <Slider /> {/* Render the Slider component */}
+      <HeadingText /> {/* Render the HeadingText component */}
+      <RecipeList /> {/* Render the RecipeList component */}
+      <Newsletter /> {/* Render the Newsletter component */}
+      <Footer /> {/* Render the Footer component */}
+    </>
   );
-
 }
