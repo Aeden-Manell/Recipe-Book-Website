8b5bf03b3280545acaf3d76d5d7f1f36b3609e48
Aeden Manell <nthabelengaedenmanell@gmail.com>
2024-04-06 19:55:59 +0200

    Comments

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,32 +1,36 @@
 import React from "react";
-import './styles/navbar-style.css';
-import { Link } from "react-router-dom";
-import logoImage from "../assets/Logo.jpeg";
+import "./styles/navbar-style.css"; // Import the CSS file for the Navbar component
+import { Link } from "react-router-dom"; // Import the Link component from react-router-dom
+import logoImage from "../assets/Logo.jpeg"; // Import the logo image
 
 const Navbar = () => {
-
+  // This component represents the Navbar of the website
 
   return (
     <div className="Navigation">
-      <nav class="flex align-center">
-        <div><img src={logoImage} alt="Logo" className="logo" /></div>
+      <nav className="flex align-center">
+        <div>
+          <img src={logoImage} alt="Logo" className="logo" />
+        </div>
         <ul>
-          <li class="big-screens">
-
-            <Link to="/">Home</Link>            
+          <li className="big-screens">
+            {/* Render the navigation links for large screens */}
+            <Link to="/">Home</Link>
             <Link to="/ourrecipes">Our Recipes</Link>
             <Link to="/ourstory">Our Story</Link>
             <Link to="/reviews">Reviews</Link>
-            <Link to="/contact" id="contact-btn">Contact</Link>
+            <Link to="/contact" id="contact-btn">
+              Contact
+            </Link>
           </li>
-          <li class="small-screens">
-            <i class="fa-solid fa-bars"></i>
+          <li className="small-screens">
+            {/* Render the hamburger menu icon for small screens */}
+            <i className="fa-solid fa-bars"></i>
           </li>
         </ul>
       </nav>
     </div>
-
   );
-}
+};
 
 export default Navbar;
