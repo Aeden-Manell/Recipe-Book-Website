038f95a8d3a51ef27080b2ff41b0645815c786ca
Rethabileenno <rethabileenno@gmail.com>
2024-04-06 14:38:52 +0200

    Added the samp page and content

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import FatCakeDetails from './fatcakedetails';
 import Tribe from './tribe';
+import SampDetails from './sampdetails';
 
 
 
@@ -17,6 +18,7 @@ function App() {
       <Routes>
         <Route path="/" element={<FatCakeDetails />} />
         <Route path="/tribe" element={<Tribe />} />
+        <Route path="/sampdetails" element={<SampDetails />} />
        
       
       </Routes>
