aec224ffb7dd904eb0d10c0adf817da51640d1c5
Aeden Manell <nthabelengaedenmanell@gmail.com>
2024-04-06 19:49:31 +0200

    Recipes Page

--- src/pages/OurRecipes.js
+++ src/pages/OurRecipes.js
@@ -1,21 +1,23 @@
-import Footer from "../components/Footer";
-import HeadingText from "../components/HeadingText";
-import HeroRecipe from "../components/HeroRecipe";
-import Navbar from "../components/Navbar";
-import RecipeList from "../components/RecipeList";
-import Newsletter from "../components/Newsletter";
+import Footer from "../components/Footer"; // Import the Footer component
+import HeadingText from "../components/HeadingText"; // Import the HeadingText component
+import HeroRecipe from "../components/HeroRecipe"; // Import the HeroRecipe component
+import Navbar from "../components/Navbar"; // Import the Navbar component
+import RecipeList from "../components/RecipeList"; // Import the RecipeList component
+import Newsletter from "../components/Newsletter"; // Import the Newsletter component
 
 const OurRecipes = () => {
-  return ( 
+  // This component represents the Our Recipes page of the website
+
+  return (
     <>
-      <Navbar/>
-      <HeroRecipe/>     
-      <HeadingText/>
-      <RecipeList/>
-      <Newsletter/>
-      <Footer/>
+      <Navbar /> {/* Render the Navbar component */}
+      <HeroRecipe /> {/* Render the HeroRecipe component */}
+      <HeadingText /> {/* Render the HeadingText component */}
+      <RecipeList /> {/* Render the RecipeList component */}
+      <Newsletter /> {/* Render the Newsletter component */}
+      <Footer /> {/* Render the Footer component */}
     </>
-   );
-}
- 
+  );
+};
+
 export default OurRecipes;
